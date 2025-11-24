987eea9155f2958494b2534a915d5488d1600b80
AmardeepZzz <solodevproject@gmail.com>
2025-11-24 20:35:54 +0530

    initial question

---
+++ src/main/java/org/solo/Main.java
@@ -0,0 +1,7 @@
+package org.solo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/solo/basic/Question1.java
@@ -0,0 +1,12 @@
+package org.solo.basic;
+
+//Loop from 1 to 100 without using number
+public class Question1 {
+
+    public static void main(String[] args) {
+
+        for(int i='A'-'@';i<'e';i++){
+            System.out.println(i);
+        }
+    }
+}
